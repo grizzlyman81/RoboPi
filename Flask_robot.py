9274e5a844e19902d217ac149c95a3a4e8dca9a2
grizzlyman81 <48761328+grizzlyman81@users.noreply.github.com>
2019-04-17 19:23:51 +0200

    Create Flask_robot.py
    Fungerar inte helt, måste fixa ”Return” satsen

---
+++ Flask_robot.py
@@ -0,0 +1,84 @@
+
+#!/usr/bin/python3
+import RPi.GPIO as GPIO
+import time
+from flask import Flask
+from flask import render_template, request
+app = Flask(__name__)
+
+GPIO.setwarnings(False)
+
+
+GPIO.setmode(GPIO.BOARD)
+
+@app.route("/")
+def index():
+    return render_template('robot.html')
+
+
+    
+def init():
+    GPIO.setup(31, GPIO.OUT)
+    GPIO.setup(33, GPIO.OUT)
+    GPIO.setup(35, GPIO.OUT)
+    GPIO.setup(37, GPIO.OUT)
+
+@app.route('/up_side')
+
+def up_side():
+    init()
+    GPIO.output(31, True)
+    GPIO.output(33, False)
+    GPIO.output(35, True)
+    GPIO.output(37, False)
+    return 'true'
+
+
+@app.route('/down_side')
+
+def down_side():
+    init()
+    GPIO.output(31, False)
+    GPIO.output(33, True)
+    GPIO.output(35, False)
+    GPIO.output(37, True)
+    
+    return 'true'
+        
+@app.route('/left_side')
+def left_side():
+    init()
+    GPIO.output(31, True)
+    GPIO.output(33, True)
+    GPIO.output(35, False)
+    GPIO.output(37, False)
+    
+    return 'true'
+
+@app.route('/right_side')
+
+def right_side():
+    init()
+    GPIO.output(31, True)
+    GPIO.output(33, False)
+    GPIO.output(35, False)
+    GPIO.output(37, True)
+    return 'true'
+    
+    
+@app.route('/stop')
+
+def stop():
+    init()
+    GPIO.output(31, False)
+    GPIO.output(33, False)
+    GPIO.output(35, False)
+    GPIO.output(37, False)
+   # GPIO.cleanup()
+    
+   # return 'true'
+
+if __name__ == "__main__":
+# print "Start"
+ app.run(host='192.168.0.191',port=5010)
+pi@RoboPi:~/robot $ 
