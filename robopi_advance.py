559a3acf4b34d30fd9d849b1a1eb9290e5c7869c
grizzlyman81 <48761328+grizzlyman81@users.noreply.github.com>
2019-05-09 20:04:20 +0200

    Update robopi_advance.py
    re-done everything NOT TESTED YET!!!
    still might need to look mote at it

--- robopi_advance.py
+++ robopi_advance.py
@@ -20,64 +20,74 @@ os.system("sudo service motion stop")
 kamera = PiCamera()
 namn = datetime.now().strftime('%c')
 
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+             
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+
+GPIO.setup(31, GPIO.OUT)
+GPIO.setup(33, GPIO.OUT)
+GPIO.setup(35, GPIO.OUT)
+GPIO.setup(37, GPIO.OUT)
+GPIO.setmode(GPIO.BOARD)
+PIN_TRIGGER = 7
+PIN_ECHO = 11
+GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+GPIO.setup(PIN_ECHO, GPIO.IN)
+
+    
+
+
+
+def forward():
+    
+    GPIO.output(31, True)
+    GPIO.output(33, False)
+    GPIO.output(35, True)
+    GPIO.output(37, False)
+
+
+def reverse():
+    
+    GPIO.output(31, False)
+    GPIO.output(33, True)
+    GPIO.output(35, False)
+    GPIO.output(37, True)
+
+def back_left():
+    
+    GPIO.output(31, False)
+    GPIO.output(33, True)
+    GPIO.output(35, False)
+    GPIO.output(37, False)
+            
+def back_right():
+    
+    GPIO.output(31, False)
+    GPIO.output(33, False)
+    GPIO.output(35, False)
+    GPIO.output(37, True)
+
+def stopit():
+    
+    GPIO.output(31, False)
+    GPIO.output(33, False)
+    GPIO.output(35, False)
+    GPIO.output(37, False)
+
+
+
 try:
 
     while True:
-        GPIO.setmode(GPIO.BOARD)
-        
-        #forward()
-        PIN_TRIGGER = 7
-        PIN_ECHO = 11
-        
-          
-            
-        def init():
-            GPIO.setup(31, GPIO.OUT)
-            GPIO.setup(33, GPIO.OUT)
-            GPIO.setup(35, GPIO.OUT)
-            GPIO.setup(37, GPIO.OUT)
-        
-        def forward():
-            init()
-            GPIO.output(31, True)
-            GPIO.output(33, False)
-            GPIO.output(35, True)
-            GPIO.output(37, False)
-
-
-        def reverse():
-            init()
-            GPIO.output(31, False)
-            GPIO.output(33, True)
-            GPIO.output(35, False)
-            GPIO.output(37, True)
-
-        def back_left():
-            init()
-            GPIO.output(31, False)
-            GPIO.output(33, True)
-            GPIO.output(35, False)
-            GPIO.output(37, False)
-            
-        def back_right():
-            init()
-            GPIO.output(31, False)
-            GPIO.output(33, False)
-            GPIO.output(35, False)
-            GPIO.output(37, True)
-
-        def stopit():
-            init()
-            GPIO.output(31, False)
-            GPIO.output(33, False)
-            GPIO.output(35, False)
-            GPIO.output(37, False)
-        
-        
-        
-        GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-        GPIO.setup(PIN_ECHO, GPIO.IN)
-
         GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
        
@@ -94,37 +104,39 @@ try:
 
         while GPIO.input(PIN_ECHO)==0:
             pulse_start_time = time.time()
+            
         while GPIO.input(PIN_ECHO)==1:
             pulse_end_time = time.time()
 
         pulse_duration = pulse_end_time - pulse_start_time
         distance = round(pulse_duration * 17150, 2)
-       
+       # print ("Distance:",distance,"cm")
        
       
             
         
             
-        if distance < 30:
-            
-            stopit()
+        if distance < 30:  // This part has stopped working
+           
+            topit()
             kamera.capture('/home/pi/robot/bilder/'+namn+'.jpeg')
             os.system("/home/pi/robot/send_mail.py")
-	    
-            
+	    #print("Distance:",distance,"cm")
+            #back_right()
             r = random.randint(0,11)
             if r > 5:
-                print("Turning right")
+                
                 back_right()
                 time.sleep(0.5)
-               
-               
-                
+            
+                        
             else:
-                print("Turning left")
+                
                 back_left()
                 time.sleep(0.5)
-                
+           
+        
+
                 
         
             
@@ -135,34 +147,26 @@ try:
             forward()
 
 
-            def getch():
-                fd = sys.stdin.fileno()
-                old_settings = termios.tcgetattr(fd)
-                try:
-                    tty.setraw(sys.stdin.fileno())
-                    ch = sys.stdin.read(1)
-             
-                finally:
-                    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-                return ch
+            
                 
                     
             char = getch() 
 
 
             if (char == "s"):
-                print("Modifing route")
+                print("Turning")
                 back_left()
                 time.sleep(0.2)
 
             elif (char == 'a'):
-                print("Modifing route")
+                print("Turning")
                 back_right()
                 time.sleep(0.2)    
 
 except KeyboardInterrupt:
-    print("Stopping robot")
+    print("Robot stopping")
     stopit()
 
     GPIO.cleanup()
-    #os.system("/home/pi/robot/send_mail.py")
+
+
